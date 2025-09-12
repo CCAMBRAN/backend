3c75fa5ebdd0bb2fe144de7aa580f5ad4f1daa3f
CCAMBRAN <seahawks.cambranis@gmail.com>
2025-09-12 17:53:03 -0600

    Update task management routes

--- routes/tareas.py
+++ routes/tareas.py
@@ -1,12 +1,29 @@
 
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from config.db import get_db_connection
 
 tareas_bp = Blueprint('tareas', __name__)
 
 @tareas_bp.route('/obtener', methods=['GET'])
 def get():
-    return jsonify({'message': 'Estas son las tareas uwu'}), 200
+    current_user = get_jwt_identity()
+    cursor= get_db_connection()
+    query = '''SELECT a.id_tarea, a.descripcion, b.name 
+                FROM tareas as a 
+                INNER JOIN usuarios as b ON a.id_usuario = b.id_usuario
+                WHERE a.id_usuario = %s'''
+    cursor.execute(query, (current_user,))
+    tareas = cursor.fetchall()
+    cursor.close()
+    if not tareas:
+        return jsonify({'message': 'No hay tareas'}), 404
+    tareas = [{'id_tarea': tarea[0], 'descripcion': tarea[1], 'usuario': tarea[2]} for tarea in tareas]
+
+    if not tareas:
+        return jsonify({'message': 'No hay tareas'}), 404
+    
+    return jsonify({'message': 'Estas son las tareas'}), 200
 
 #crear endpoint con post recibiendo datos desde body
 
