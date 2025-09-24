98c07f5e3398bc2c845445fb2a6292bbd79ab035
CCAMBRAN <seahawks.cambranis@gmail.com>
2025-09-24 17:19:29 -0600

    commit

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app = create_app()
 if __name__ == "__main__":
 
     #Obtenemos el puerto
-    port = int(os.getenv("PORT",8080))
+    port = int(os.getenv("PORT",3307))
 
     #Corremos la app
     app.run(host="0.0.0.0", port=port, debug=True)
